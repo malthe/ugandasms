4bd87ce2fcd00b2d730899deadb5530acba149c8
Malthe Borch <mborch@gmail.com>
2010-04-16 17:10:03 +0300

    Assign polymorphic identity to all messages; this just allows us to not have to add this manually to every message class, since we define the message kind (string) from the class name anyway.

--- src/protocol/messages.py
+++ src/protocol/messages.py
@@ -1,9 +1,16 @@
+import re
+
 from sqlalchemy.orm import relation
 from sqlalchemy.orm import backref
 
 from router.models import Message
 
 from .models import User
+
+def camelcase_to_underscore(str):
+    return re.sub(
+        '(((?<=[a-z])[A-Z])|([A-Z](?![A-Z]|$)))', '_\\1',
+        str).lower().strip('_')
 
 class UserMessage(Message):
     user = relation(
@@ -23,3 +30,9 @@ class Register(UserMessage):
 
 class Approve(UserMessage):
     """Approve user to join group."""
+
+for cls in locals().values():
+    if isinstance(cls, type) and issubclass(cls, Message):
+        args = cls.__mapper_args__ = cls.__mapper_args__.copy()
+        kind = cls.kind = camelcase_to_underscore(cls.__name__)
+        args['polymorphic_identity'] = kind
