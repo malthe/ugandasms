174ebd3b441bed5d2b16796afb8ed033ec082b84
Malthe Borch <mborch@gmail.com>
2010-04-17 12:43:09 +0300

    Subclass to conform with message recording policy.

--- src/router/tests/test_dispatch.py
+++ src/router/tests/test_dispatch.py
@@ -9,8 +9,11 @@ class HandlerTest(FunctionalTestCase):
 
     def test_message_record(self):
         from ..models import Message
+        class Test(Message):
+            pass
+
         self.handler(
-            Message("test", sender="123", receiver="456"))
+            Test("test", sender="123", receiver="456"))
 
         from ..orm import Session
         session = Session()
@@ -19,5 +22,5 @@ class HandlerTest(FunctionalTestCase):
         self.assertEquals(len(results), 1)
         self.assertEquals(results[0].text, u"test")
         self.assertEquals(results[0].sender, u"123")
-        self.assertEquals(results[0].kind, None)
-        self.assertEquals(results[0].reply, u"No handler available for message kind ``message``.")
+        self.assertEquals(results[0].kind, 'test')
+        self.assertEquals(results[0].reply, u"No handler available for message kind ``test``.")
