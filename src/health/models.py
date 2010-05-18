63519231c844ff3639eeed712961efadd3d918b1
Malthe Borch <mborch@gmail.com>
2010-05-18 10:53:55 +0300

    Completed epidemiological reporting functionality.

--- src/health/models.py
+++ src/health/models.py
@@ -1,3 +1,5 @@
+import itertools
+
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from polymorphic import PolymorphicModel as Model
@@ -6,14 +8,14 @@ from router.models import User
 
 from picoparse import remaining
 from picoparse import one_of
+from picoparse import optional
+from picoparse import partial
 from picoparse import peek
 from picoparse.text import whitespace
 from picoparse.text import whitespace1
 from picoparse.text import caseless_string
 from router.parser import one_of_strings
-from router.parser import next_parameter
 from router.parser import digits
-from router.parser import float_digits
 from router.parser import ParseError
 
 from polymorphic.manager import PolymorphicManager
@@ -42,22 +44,42 @@ class Subscription(Model):
 class Aggregate(Model):
     code = models.CharField(max_length=2, db_index=True)
     time = models.DateTimeField(db_index=True)
-    value = models.FloatField()
+    user = models.ForeignKey(User)
+    value = models.IntegerField()
+
+    class Meta:
+        ordering = ['-id']
 
 class Epi(Incoming):
     """Report on epidemiological data.
 
     Regular reports should come in with the format::
 
-      +EPI [<token> <value>]+
+      +EPI [<token> <integer_value>]*
 
-    Example input for 12 cases of malaria and 4 tuberculous cases.
+    The ``token`` must be one of the keys defined in
+    ``TOKENS``. Negative values are not allowed.
+
+    Example input for 12 cases of malaria and 4 tuberculous cases::
 
       +EPI MA 12, TB 4
 
+    The reports are confirmed in a message reply, along with
+    percentage or absolute change (whichever is applicable depending
+    on whether this or the previous value is zero) on consecutive
+    reporting.
+
+    Example output::
+
+      You reported malaria 12 (+5) and tuberculosis 4 (+23%).
+
+    All aggregates are entered into the database as separate
+    objects. To group aggregates based on reports, filter by user and
+    group by time.
     """
 
     TOKENS = {
+        'BD': 'Bloody diarrhea',
         'MA': 'Malaria',
         'TB': 'Tuberculosis',
         }
@@ -83,9 +105,14 @@ class Epi(Incoming):
 
                 whitespace1()
                 try:
-                    value = float("".join(float_digits()))
+                    minus = optional(partial(one_of, '-'), '')
+                    value = int("".join([minus]+digits()))
                 except:
                     raise ParseError("Expected a value for %s." % code)
+
+                if value < 0:
+                    raise ParseError("Got %d for %s. You must report a positive value." % (
+                        value, cls.TOKENS[code].lower()))
 
                 aggregates[code] = value
                 whitespace()
@@ -93,6 +120,37 @@ class Epi(Incoming):
         return {
             'aggregates': aggregates
             }
+
+    def handle(self, aggregates={}):
+        if self.user is None: # pragma: NOCOVER
+            self.reply(u"Please register before sending in reports.")
+        elif aggregates:
+            stats = []
+            for code, value in sorted(aggregates.items()):
+                stat = "%s %d" % (self.TOKENS[code].lower(), value)
+                previous = Aggregate.objects.filter(code=code, user=self.user)
+                if len(previous):
+                    aggregate = previous[0]
+                    if value > 0 and aggregate.value > 0:
+                        ratio = 100 * (float(value)/aggregate.value - 1)
+                        r = "%1.f%%" % abs(ratio)
+                    else:
+                        ratio = value-aggregate.value
+                        r = str(abs(ratio))
+                    if ratio > 0:
+                        r = "+" + r
+                    else:
+                        r = "-" + r
+                    stat += " (%s)" % r
+                Aggregate(code=code, value=value, time=self.time, user=self.user).save()
+                stats.append(stat)
+            sep = [", "] * len(stats)
+            if len(stats) > 1:
+                sep[-2] = " and "
+            sep[-1] = ""
+            self.reply(u"You reported %s." % "".join(itertools.chain(*zip(stats, sep))))
+        else:
+            self.reply(u"Please include one or more reports.")
 
 class Signup(Incoming):
     """Message to register as health worker.
@@ -126,9 +184,7 @@ class Signup(Incoming):
 
     def handle(self, role=None, code=None):
         if self.user is None:
-            self.reply(getattr(
-                settings, "REGISTERED_USERS_ONLY_MESSAGE",
-                u"You must be registered to use this service."))
+            self.reply(u"Please register before signing up for this service.")
         else:
             try:
                 facility = Facility.objects.filter(code=code).get()
--- src/health/tests/test_epi.py
+++ src/health/tests/test_epi.py
@@ -1,3 +1,5 @@
+import datetime
+
 from router.testing import FunctionalTestCase
 from router.testing import UnitTestCase
 
@@ -25,6 +27,10 @@ class ParserTest(UnitTestCase):
         model, data = self._epi("+epi MA 5")
         self.assertEqual(data['aggregates'], {'MA': 5.0})
 
+    def test_negative_value(self):
+        from router.parser import ParseError
+        self.assertRaises(ParseError, self._epi, "+epi MA -5")
+
     def test_values(self):
         model, data = self._epi("+epi MA 5 TB 10")
         self.assertEqual(data['aggregates'], {'MA': 5.0, 'TB': 10.0})
@@ -46,8 +52,9 @@ class HandlerTest(FunctionalTestCase): # pragma: NOCOVER
 
     @staticmethod
     def _handle(model, uri="test://old", text="", **kwargs):
+        TIME = datetime.datetime(1999, 12, 31, 0, 0, 0)
         from router.models import Peer
-        message = model(text=text)
+        message = model(text=text, time=TIME)
         message.peer, created = Peer.objects.get_or_create(uri=uri)
         message.peer.save()
         message.save()
@@ -60,11 +67,48 @@ class HandlerTest(FunctionalTestCase): # pragma: NOCOVER
         return cls._handle(Registration, **kwargs)
 
     @classmethod
-    def _signup(cls, **kwargs):
-        from ..models import Signup
-        return cls._handle(Signup, **kwargs)
-
-    @classmethod
     def _epi(cls, **kwargs):
-        from ..models import Signup
-        return cls._handle(Signup, **kwargs)
+        from ..models import Epi
+        return cls._handle(Epi, **kwargs)
+
+    def test_no_reports(self):
+        self._register(name="foo")
+        message = self._epi(aggregates={})
+        from ..models import Aggregate
+        self.assertEqual(Aggregate.objects.count(), 0)
+        self.assertEqual(message.replies.count(), 1)
+
+    def test_single_report(self):
+        self._register(name="foo")
+        message = self._epi(aggregates={'MA': 5})
+        from ..models import Aggregate
+        self.assertEqual(Aggregate.objects.count(), 1)
+        self.assertEqual(Aggregate.objects.get().user, message.user)
+        reply = message.replies.get()
+        self.assertTrue('malaria 5' in reply.text)
+
+    def test_follow_up_reports(self):
+        self._register(name="foo")
+        self._epi(aggregates={'MA': 5})
+        update1 = self._epi(aggregates={'MA': 10})
+        update2 = self._epi(aggregates={'MA': 8})
+        self.assertTrue('malaria 10 (+100%)' in update1.replies.get().text)
+        self.assertTrue('malaria 8 (-20%)' in update2.replies.get().text)
+
+    def test_follow_up_zero(self):
+        self._register(name="foo")
+        self._epi(aggregates={'MA': 5})
+        update1 = self._epi(aggregates={'MA': 0})
+        update2 = self._epi(aggregates={'MA': 10})
+        self.assertTrue(
+            'malaria 0 (-5)' in update1.replies.get().text, update1.replies.get().text)
+        self.assertTrue(
+            'malaria 10 (+10)' in update2.replies.get().text, update2.replies.get().text)
+
+    def test_multiple_reports(self):
+        self._register(name="foo")
+        message = self._epi(aggregates={'MA': 5, 'TB': 10, 'BD': 2})
+        from ..models import Aggregate
+        self.assertEqual(Aggregate.objects.count(), 3)
+        reply = message.replies.get()
+        self.assertTrue('bloody diarrhea 2, malaria 5 and tuberculosis 10' in reply.text)
