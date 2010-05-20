387c4c3906e6ed25fb153dea6f625bf09655f97d
Malthe Borch <mborch@gmail.com>
2010-05-20 16:08:23 +0300

    Adding management commands for trying out parsing and handling from the command-line.

---
+++ src/router/management/commands/parse.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from router.transports import Transport
+from router.parser import ParseError
+from pprint import pformat
+
+class Command(BaseCommand):
+    args = 'text'
+    help = 'Parses the provided text message'
+
+    def handle(self, text, **options):
+        transport = Transport("script")
+        messages = []
+        while text:
+            try:
+                model, data, text = transport.parse(text)
+            except ParseError, error:
+                print error
+            else:
+                messages.append((model, data))
+
+        justification = max([len(model.__name__) for (model, data) in messages])
+        for model, data in messages:
+                print "%s: %s" % (model.__name__.ljust(justification), pformat(data))
