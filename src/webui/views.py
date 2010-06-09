567e132145c4a934460f06b53f24d4b4d22837ca
Malthe Borch <mborch@gmail.com>
2010-06-09 10:27:00 +0300

    Generate URLs using the URL tag to accomodate vhost configurations.

--- src/webui/views.py
+++ src/webui/views.py
@@ -4,7 +4,11 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 
 @login_required
 def dashboard(request):
     return render_to_response("dashboard.html", {}, RequestContext(request))
+
+def static(request):
+    raise ObjectDoesNotExist(request.path)
