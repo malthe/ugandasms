fff2877dc2eee061a774a349d55b4e72b6db975a
Malthe Borch <mborch@gmail.com>
2010-06-19 15:57:30 +0300

    Update to allow out-of-order parent references.

--- src/cvs/management/commands/import_locations.py
+++ src/cvs/management/commands/import_locations.py
@@ -2,14 +2,21 @@ import csv
 import sys
 import pyproj
 import traceback
+import collections
 
 from django.core.management.base import BaseCommand
+from django.template.defaultfilters import slugify as _slugify
 from location.models import LocationKind
 from location.models import Area
 from location.models import Facility
 
 wgs84 = pyproj.Proj(proj='latlong', datum='WGS84')
 utm33 = pyproj.Proj(proj='utm', zone='33')
+
+Entry = collections.namedtuple("Entry", "code, name, level, itp_otc_status, distribution_day, iycf, x, y, reports_to, district, county, sub_county, parish, village, sub_village, household, house_holds, population, needed_vhts, trained_vhts, vhts_to_be_trained")
+
+def slugify(string):
+    return _slugify(string).replace('-', '')
 
 class Command(BaseCommand):
     args = 'path'
@@ -18,11 +25,10 @@ class Command(BaseCommand):
     def handle(self, path, **options):
         areas = Area.objects.count()
         facilities = Facility.objects.count()
-        print "%d existing areas." % areas
-        print "%d existing facilities." % facilities
+        print >> sys.stderr, "%d existing areas." % areas
+        print >> sys.stderr, "%d existing facilities." % facilities
 
         reader = csv.reader(open(path), delimiter=',', quotechar='"')
-        parent = None
 
         _k_country = LocationKind.objects.get(name='Country')
         _k_district = LocationKind.objects.get(slug="district")
@@ -38,69 +44,102 @@ class Command(BaseCommand):
             root = Area.add_root(name="Uganda", kind=_k_country)
 
         reader.next()
-        for index, line in enumerate(reader):
+
+        # read all entries
+        entries = []
+        for line in reader:
             line.extend(
                 [""]*(21-len(line)))
-            try:
-                code, name, level, itp_otc_status, distribution_day, iycf, x, y, parent_composite, district, county, sub_county, parish, village, sub_village, household, house_holds, population, needed_vhts, trained_vhts, vhts_to_be_trained = line
-            except Exception, exc:
-                print traceback.format_exc(exc)
-                continue
 
-            if x and y:
-                transformed = pyproj.transform(utm33, wgs84, float(x), float(y))
+            try:
+                entry = Entry(*line)
+            except Exception, exc:
+                print >> sys.stderr, traceback.format_exc(exc)
+            else:
+                entries.append(entry)
+
+        # read all facilities
+        facility_entries = {}
+        for entry in entries:
+            if entry.name and entry.level:
+                name = slugify(entry.name)
+                level = slugify(entry.level)
+                facility_entries[name, level] = entry
+
+        created = {}
+        def get_or_create(entry):
+            try:
+                return Facility.objects.get(code=entry.code)
+            except Facility.DoesNotExist:
+                pass
+
+            parent = None
+
+            if entry.reports_to:
+                name, level = entry.reports_to.rsplit(' ', 1)
+
+                parent = created.get((name, level))
+                if parent is None:
+                    try:
+                        parent_entry = facility_entries[slugify(name), slugify(level)]
+                    except KeyError:
+                        raise KeyError("Facility not found: '%s %s'." % (name, level))
+
+                    try:
+                        parent = created[name, level] = get_or_create(parent_entry)
+                    except RuntimeError:
+                        raise RuntimeError(
+                            "Recursive definition: %s %s => %s %s." % (
+                                entry.code, entry.name,
+                                parent_entry.code, parent_entry.name))
+
+                create = parent.get().add_child
+            else:
+                create = Facility.add_root
+
+            if entry.x and entry.y:
+                transformed = pyproj.transform(
+                    utm33, wgs84, float(entry.x), float(entry.y))
                 longitude, latitude = map(str, transformed)
             else:
                 longitude, latitude = None, None
 
-            # create new facility if required
+            level = entry.level.replace(' ', '')
+            slug = level.lower()
             try:
-                facility = Facility.objects.get(code=code)
-            except Facility.DoesNotExist:
-                if level:
-                    level = level.replace(' ', '')
-                    try:
-                        slug = level.lower()
-                        kind = LocationKind.objects.get(slug=slug)
-                    except LocationKind.DoesNotExist:
-                        print "%03d Skipping row "
-                        "(unable to look up location kind '%s')" % (
-                            index+2, level)
-                    else:
-                        if parent_composite:
-                            n, l = parent_composite.rsplit(' ', 1)
-                            try:
-                                parent = Facility.objects.get(
-                                    name=n, kind__slug=l.lower())
-                            except Facility.DoesNotExist:
-                                print "%03d Skipping row (unable to find " \
-                                      "reporting facility with name '%s')." % (
-                                    index+2, n)
-                                continue
+                kind = LocationKind.objects.get(slug=slug)
+            except LocationKind.DoesNotExist:
+                raise LocationKind.DoesNotExist(slug)
 
-                            create = parent.add_child
-                        else:
-                            create = Facility.add_root
+            facility = create(
+                name=entry.name,
+                code=entry.code,
+                kind=kind,
+                longitude=longitude,
+                latitude=latitude,
+                )
 
-                        facility = create(
-                            name=name,
-                            kind=kind,
-                            code=code,
-                            longitude=longitude,
-                            latitude=latitude,
-                            )
+            facility = Facility.objects.get(pk=facility.pk)
+            return facility
 
-                        # explicit reload of non-trivial parent
-                        if parent is not None:
-                            parent = Facility.objects.get(pk=parent.pk)
-
+        facility = None
+        for entry in entries:
             # update locations
-            locations = ((district, _k_district),
-                         (county, _k_county),
-                         (sub_county, _k_sub_county),
-                         (parish, _k_parish),
-                         (village, _k_village),
-                         (sub_village, _k_sub_village))
+            locations = ((entry.district, _k_district),
+                         (entry.county, _k_county),
+                         (entry.sub_county, _k_sub_county),
+                         (entry.parish, _k_parish),
+                         (entry.village, _k_village),
+                         (entry.sub_village, _k_sub_village))
+
+            if entry.code:
+                try:
+                    facility = get_or_create(entry)
+                except RuntimeError, error:
+                    print >> sys.stderr, "Warning: %s" % error
+                    continue
+            elif facility is None:
+                continue
 
             parent = root
             for placename, kind in locations:
